a9c600f797828a3fd963b7a9227e26189a551539
Gorky8685 <826769080@qq.com>
2024-04-08 00:56:49 +0800

    test2test

--- src/pyotm/test2.py
+++ src/pyotm/test2.py
@@ -1,12 +1,21 @@
 from pyotm.OTMWrapper import OTMWrapper
 import numpy as np
-
+import pandas as pd
 if "otm" in locals():
 	del otm
 
-otm = OTMWrapper("intersection.xml")
+otm = OTMWrapper("line_ctm.xml")
 otm.run(start_time=0,duration=2500,output_dt=10)
 Y = otm.get_state_trajectory()
 print(Y.keys())
+np.savez(r'C:\Users\MECHREVO\Desktop\Y.npz')
+writer = pd.ExcelWriter('C:/Users/MECHREVO/Desktop/Y.xlsx')
 
+# Convert each array in Y to a DataFrame and write to the Excel file
+for key, array in Y.items():
+    df = pd.DataFrame(array)
+    df.to_excel(writer, sheet_name=key)
+
+# Close the Pandas Excel writer and save the Excel file to disk
+writer.save()
 del otm
